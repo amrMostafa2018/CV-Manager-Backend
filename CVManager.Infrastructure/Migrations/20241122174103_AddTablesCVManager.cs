3a29a24a452ba4bcaf58594c03244569b49451ad
Amr.Mostafa <Amr.Mostafa@AMRMOSTAFA>
2024-11-22 19:48:54 +0200

    Add Migrations

---
+++ CVManager.Infrastructure/Migrations/20241122174103_AddTablesCVManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTablesCVManager : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CVs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedById = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedByName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedById = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModifiedByName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CVs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExperienceInformation",
+                columns: table => new
+                {
+                    ExperienceInformationId = table.Column<int>(type: "int", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CompanyField = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExperienceInformation", x => x.ExperienceInformationId);
+                    table.ForeignKey(
+                        name: "FK_ExperienceInformation_CVs_ExperienceInformationId",
+                        column: x => x.ExperienceInformationId,
+                        principalTable: "CVs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonalInformation",
+                columns: table => new
+                {
+                    PersonalInformationId = table.Column<int>(type: "int", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CityName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    MobileNumber = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalInformation", x => x.PersonalInformationId);
+                    table.ForeignKey(
+                        name: "FK_PersonalInformation_CVs_PersonalInformationId",
+                        column: x => x.PersonalInformationId,
+                        principalTable: "CVs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExperienceInformation");
+
+            migrationBuilder.DropTable(
+                name: "PersonalInformation");
+
+            migrationBuilder.DropTable(
+                name: "CVs");
+        }
+    }
+}
